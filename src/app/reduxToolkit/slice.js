a3c242d6e71dc8dbb18c853486dfb849ca9b4f0e
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-07-26 10:40:19 +0530

    Slice created.

---
+++ src/app/reduxToolkit/slice.js
@@ -0,0 +1,22 @@
+const { default: AddEmployees } = require("@/components/AddEmployees")
+const {createSlice, nanoid} = require("@reduxjs/toolkit")
+
+const startingState = {
+    employees:[]
+}
+
+const Slice = createSlice({
+    startingState,
+    reducers:{
+        addEmployee:(state, action)=>{
+            const data = {
+                id:nanoid(),
+                name:action.name
+            }
+            state.employees.push(data)
+        }
+    }
+});
+
+export const {addEmployee} = Slice.actions;
+export default Slice.reducer;
